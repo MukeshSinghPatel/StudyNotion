7d9d08b2a8e8e64b951500de59f1d3ab17aeab3e
Mukesh Singh Patel <149561363+MukeshSinghPatel@users.noreply.github.com>
2024-05-24 15:03:49 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -2,6 +2,6 @@ const Razorpay = require("razorpay");
 
 
 exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    key_secret: process.env.RAZORPAY_SECRET,
+    key_id: 'rzp_test_C8YsZwKnR3PcHi', //process.env.RAZORPAY_KEY,
+    key_secret: 'Nfj2MR8ZZ8RubznujsQwlCvq' //process.env.RAZORPAY_SECRET,
 });
